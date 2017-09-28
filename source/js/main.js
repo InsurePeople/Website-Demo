b0db16df8c995e672dc52474fcb587abde4e28ea
David <a.dave.fred@gmail.com>
2017-09-28 19:39:43 +0100

    Update main.js

--- source/js/main.js
+++ source/js/main.js
@@ -3,14 +3,14 @@ $("document").ready(function(){
         if(sessionStorage.getItem('username') == null){
             window.location.replace("log-in.html?" + $(location).attr('href'));
         } else if(sessionStorage.getItem('idval') == null){
-            if(window.location.href != "/Website-Demo/get-started.html"){
+            if(window.location.pathname != "/Website-Demo/get-started.html"){
                 //window.location.replace("get-started.html");
-                console.log(window.location.href);
+                console.log(window.location.pathname);
         }
         } else if(sessionStorage.getItem('phone') == null){
             if(window.location.pathname != "/Website-Demo/get-started.html" && window.location.pathname != "/Website-Demo/insureme.html"){
                 //window.location.replace("insureme.html");
-                console.log(window.location.href);
+                console.log(window.location.pathname);
             }
         }
     }
