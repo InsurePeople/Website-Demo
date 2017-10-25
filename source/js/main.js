6e1681df3d0f4d0aaf77a943984d60af7cf2f269
David <a.dave.fred@gmail.com>
2017-10-25 17:49:26 +0100

    JS update
    Fixed JS problems in new style, updated policy pages in new style.

--- source/js/main.js
+++ source/js/main.js
@@ -1,25 +1,22 @@
-// $("document").ready(function(){
-//     // if(window.location.pathname != "/Website-Demo/" && window.location.pathname != "/Website-Demo/index.html" && window.location.pathname != "/Website-Demo/log-in.html")
-//     // {
-//     //     if(window.sessionStorage){
-//     //         if(sessionStorage.getItem('username') == null){
-//     //             window.location.replace("log-in.html?" + $(location).attr('href'));
-//     //         } else if(sessionStorage.getItem('idval') == null){
-//     //             if(window.location.pathname != "/Website-Demo/get-started.html"){
-//     //                 window.location.replace("get-started.html");
-//     //             }
-//     //         } else if(sessionStorage.getItem('phone') == null){
-//     //             if(window.location.pathname != "/Website-Demo/get-started.html" && window.location.pathname != "/Website-Demo/insureme.html"){
-//     //                 window.location.replace("insureme.html");
-//     //             }
-//     //         }
-//     //     }
-//     // }
-//     setTimeout(function(){ $(".popuppointer").focus(); }, 2000);
-//     $(".popuppointer").focus(function(){
-//        $(".popuphelp").fadeIn();
-//     });
-//     $(".popuppointer").focusout(function(){
-//        $(".popuphelp").fadeOut();
-//     });
-// });
+ $("document").ready(function(){
+     if(window.sessionStorage){
+         if(sessionStorage.getItem('username') == null){
+              sessionStorage.setItem('username', "aaa");
+         }
+         if(sessionStorage.getItem('idval') == null){
+              sessionStorage.setItem('idval', Math.floor(Math.random() * 3));
+              sessionStorage.setItem('socval', Math.floor(Math.random() * 3));
+         }
+         if(sessionStorage.getItem('phone') == null){
+              sessionStorage.setItem('phone', "iPhone");
+              sessionStorage.setItem('total', 5);
+         }
+      }
+     setTimeout(function(){ $(".popuppointer").focus(); }, 2000);
+     $(".popuppointer").focus(function(){
+        $(".popuphelp").fadeIn();
+     });
+     $(".popuppointer").focusout(function(){
+        $(".popuphelp").fadeOut();
+     });
+ });
